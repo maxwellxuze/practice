2df69e46a88c0b4338ff7a773d1ca24b11af2e0d
maxwellxu <xuze@seu.edu.cn>
2019-08-23 16:15:12 +0800

    增加基础

--- note_python.py
+++ note_python.py
@@ -2,6 +2,24 @@
 import numpy as np
 import panda as pd
 
+#基础
+'''
+range(stop)
+range(start, stop[, step])
+start：计数从 start 开始。默认是从 0 开始。例如range（5）等价于range（0， 5）;
+stop：计数到 stop 结束，但不包括 stop。例如：range（0， 5） 是[0, 1, 2, 3, 4]没有5
+step：步长，默认为1。例如：range（0， 5） 等价于 range(0, 5, 1)
+'''
+#循环
+for i in range(5) #0,1,2,3,4
+for i in range(3,10,2) # 3,5,7,9
+
+#条件
+if : #注意条件后面加冒号
+elif:
+else:
+
+
 a=np.array([[1,2,3],[4,5,6]])
 a.shape #矩阵大小
 a[0:5,:]  a[0:5] #前5行
@@ -13,13 +31,15 @@ a**3 #a3次方
  a[-1]最后一行
  a[1:]从1到最后一行
  a[1：-1]从1行到倒数第二行
- 即：右边为开区间
+ 即：右边为开区
  '''
+ 
  a[[2,3,6,5]] #数组a中第2,3,6,5行
  a[[2,3],[4,5]]#数组中[2,3],[3,5]行元素
  
- #区别DateFrame与Series
+ #区别DateFrame与Series********************************************************
  #Series只传入一个字典，则索引为原字典的键！！！！
+ #Series只有索引，没有columns.
 
  series1 = Series({'':VALUE,...},index=['','',''])
  #DateFrame由多个series组成的列，共用一个索引index
@@ -27,7 +47,6 @@ a**3 #a3次方
  
  pd.null(obj2)#检测空值
  frame = DateFrame({'Name':{1:'tim',2:'tom'}})#'Name'为列，内层键为索引
- 
  
  
  
@@ -57,4 +76,4 @@ a**3 #a3次方
  
  #matplotlib*******************************************************
  #如果为plot()命令提供单个列表或数组，则matplotlib假定它是一系列y值，并自动为您生成x值。
- 
+ pip --default-timeout=100 install -U Pillow #设置延迟时间
